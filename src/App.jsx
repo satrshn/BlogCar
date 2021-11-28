f2f6b38b18d3fc845bd9709a5d2c347df61d3c52
lolyep43 <lolyep43@gmail.com>
2021-11-29 00:46:54 +0800

    Gak tahu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import React from 'react';
+import Topbar from './Components/Topbar/Topbar';
+import About from './Menu/About/About';
+import Produk from './Menu/Produk/Produk';
+import Home from './Menu/Home/Home';
+import { Routes, Route } from 'react-router-dom';
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Topbar />
+      <Home />
+      <Produk />
+      {/* <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Routes> */}
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
